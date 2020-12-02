4d528f51d5224738a9830c28d9475a4440d77928
Sławomir Śpiewak <slawekwaga@gmail.com>
2020-12-02 12:44:38 +0100

    comments

--- include/valve-bsp-parser/core/requirements.hpp
+++ include/valve-bsp-parser/core/requirements.hpp
@@ -21,7 +21,7 @@
 
 // check for nodiscard
 
-#if __cplusplus >= 201703L
+#if __cplusplus >= 201703L // in MSVC this variable is bogus and refers to C++98 version.
     #define NODISCARD [[nodiscard]]
 #elif defined(_MSC_VER)
     #define NODISCARD _NODISCARD
